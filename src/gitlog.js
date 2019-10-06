613305e07a21d60b75201dc232563dfe0cb53272
Igor Kling <klingiv@altarix.ru>
2019-10-06 23:44:24 +0400

    styling

--- src/gitlog.js
+++ src/gitlog.js
@@ -1,69 +1,95 @@
 import React, { useRef, useEffect } from 'react'
+import styled from 'styled-components'
+import graph from './graph'
 
-const X_STEP = 10
-const Y_STEP = 30
+const ROW_HEIGHT = 25
+const X_STEP = 15
+const Y_STEP = ROW_HEIGHT
 
-const CANVAS_WIDTH = 512
-const CANVAS_HEIGHT = 512
 const LINE_WIDTH = 2
 const COMMIT_RADIUS = 5
 
-const branchColor = branch => {
-  return (
-    [
-      '#0098d4',
-      '#b36305',
-      '#e32017',
-      '#ffd300',
-      '#00782a',
-      '#f3a9bb',
-      '#a0a5a9',
-      '#9b0056',
-      '#003688',
-      '#000000',
-      '#95cdba',
-      '#00a4a7',
-      '#ee7c0e',
-      '#84b817'
-    ][branch] || 'black'
-  )
-}
+// TODO: высчитывать динамически в зависимости от максимального кол-ва || веток (процессинг только это покажет)
+const CANVAS_WIDTH = 500
+const CANVAS_HEIGHT = 500
 
-const yPositionForIndex = yIndex => {
-  return (yIndex + 0.5) * Y_STEP
-}
+const NO_BRANCH_COLOR = '#a0a5a9'
 
-const xPositionForIndex = xIndex => {
-  // Subtract the position from the width to right-align our elements
-  // return CANVAS_WIDTH - (xIndex + 1) * X_STEP
+const colors = [
+  '#84b817',
+  '#e32017',
+  '#ee7c0e',
+  '#ffd300',
+  '#f3a9bb',
+  '#b36305',
+  '#00a4a7',
+  '#00782a',
+  '#003688',
+  '#9b0056',
+  '#95cdba'
+]
 
-  return (xIndex + 1) * X_STEP
-}
+const CanvasStyle = styled.canvas`
+  z-index: 9999;
+  position: absolute;
+  left: 0px;
+  top: 0px;
+  pointer-events: none;
+`
 
-const drawCommit = (ctx, commit, yIndex) => {
-  // Thicker lines for the circles, or they look odd
-  ctx.lineWidth = LINE_WIDTH * 2
+const fillRoutes = (from, to, iterable) => iterable.map((branch, index) => [from(index), to(index), branch])
+const I = i => i
 
-  var x = xPositionForIndex(commit.position), // Positioning of commit circle
-    y = yPositionForIndex(yIndex),
-    innerRadius = COMMIT_RADIUS - LINE_WIDTH
+const branchColor = branch => colors[branch % 11] || 'black'
 
-  ctx.fillStyle = '#ffffff'
-  ctx.strokeStyle = '#000000'
+const yPositionForIndex = yIndex => (yIndex + 0.5) * Y_STEP
+
+const xPositionForIndex = xIndex => (xIndex + 1) * X_STEP
+
+const drawCommit = (ctx, topOffset, commit, yIndex) => {
+  const { sha, offset, isHead, branch } = commit
+
+  const x = xPositionForIndex(offset) // Positioning of commit circle
+  const y = yPositionForIndex(yIndex) + topOffset
+  const innerRadius = COMMIT_RADIUS - LINE_WIDTH - (!sha || isHead ? 1 : 0)
+
+  // ctx.fillStyle = !sha || isHead ? '#ffffff' : branchColor(branch)
+  // ctx.strokeStyle = sha ? branchColor(branch) : NO_BRANCH_COLOR
+  // ctx.lineWidth = !sha || isHead ? 8 : LINE_WIDTH * 2 - 1 // + (!sha ? 2 : 0)
+
+  ctx.fillStyle = branchColor(branch)
+  ctx.strokeStyle = '#fff' // соответствует цвету фона!!!
+  ctx.lineWidth = 2
+
   ctx.beginPath()
-  ctx.arc(x, y, innerRadius, 0, 2 * Math.PI) // Draw a circle
+  // ctx.arc(x, y, innerRadius, 0, 2 * Math.PI) // Draw a circle
+  ctx.arc(x, y, 4.5, 0, 2 * Math.PI) // Draw a circle
+  ctx.closePath()
   ctx.stroke() // Draw the outer line
   ctx.fill() // Fill the inner circle
+
+  // ctx.fillStyle = '#000'
+  // ctx.strokeStyle = '#000'
+  // ctx.font = '12px Arial'
+  // ctx.fillText(sha, x + 8, y + 4)
 }
 
-const drawRoute = (ctx, route, yIndex) => {
-  var fromX = xPositionForIndex(route.from), // Starting position for route
-    fromY = yPositionForIndex(yIndex),
-    toX = xPositionForIndex(route.to), // Ending position for route
-    toY = yPositionForIndex(yIndex + 1) // НЕ ВЕРНО!!!
+const drawRoute = (ctx, topOffset, route, commit, yIndex) => {
+  const { sha } = commit
+  const [from, to, branch] = route
 
-  ctx.strokeStyle = branchColor(route.branch) // Gets a colour based on the branch no.
-  ctx.lineWidth = LINE_WIDTH
+  // Starting position for route
+  const fromX = xPositionForIndex(from)
+  const fromY = yPositionForIndex(yIndex) + topOffset
+
+  // Ending position for route
+  const toX = xPositionForIndex(to)
+  const toY = yPositionForIndex(yIndex + 1) + topOffset
+
+  ctx.strokeStyle = sha ? branchColor(branch) : NO_BRANCH_COLOR // Gets a colour based on the branch no.
+  // ctx.fillStyle = sha ? branchColor(branch) : NO_BRANCH_COLOR // Gets a colour based on the branch no.
+
+  ctx.lineWidth = 2 //LINE_WIDTH
 
   ctx.beginPath()
   ctx.moveTo(fromX, fromY) // Place the cursor at the start point
@@ -71,150 +97,105 @@ const drawRoute = (ctx, route, yIndex) => {
   if (fromX === toX) {
     ctx.lineTo(toX, toY) // Draw a line to the finish point
   } else {
-    ctx.bezierCurveTo(fromX - X_STEP / 4, fromY + Y_STEP / 2, toX + X_STEP / 4, toY - Y_STEP / 2, toX, toY)
+    ctx.bezierCurveTo(fromX - X_STEP / 4, fromY + Y_STEP / 1.5, toX + X_STEP / 4, toY - Y_STEP / 1.5, toX, toY)
   }
 
+  // ctx.fill()
   ctx.stroke()
 }
 
-const drawGraph = (ctx, graphNodes) => {
-  graphNodes.forEach((node, yIndex) => {
+const drawGraph = (ctx, topOffset, nodes) => {
+  nodes.forEach((node, yIndex) => {
     // Draw the routes for this node
-    node.routes.forEach(route => drawRoute(ctx, route, yIndex))
+    node.routes.forEach(route => drawRoute(ctx, topOffset, route, node, yIndex))
 
     // Draw the commit on top of the routes
-    drawCommit(ctx, node, yIndex)
+    drawCommit(ctx, topOffset, node, yIndex)
   })
 }
-/*
-const graph = [
-  {
-    position: 0, // X-axis location for the commit in this row
-    routes: [
-      {
-        from: 0, // X-axis of the starting location for the branch line
-        to: 1, // X-axis for the finishing location for the branch line
-        branch: 0 // Branch number (used for colouring the line)
-      }
-    ]
-  },
-  {
-    position: 1, // X-axis location for the commit in this row
-    routes: [
-      {
-        from: 1, // X-axis of the starting location for the branch line
-        to: 1, // X-axis for the finishing location for the branch line
-        branch: 0 // Branch number (used for colouring the line)
-      }
-    ]
-  }
-  // {
-  //   position: 1, // X-axis location for the commit in this row
-  //   routes: [
-  //     {
-  //       from: 1, // X-axis of the starting location for the branch line
-  //       to: 1, // X-axis for the finishing location for the branch line
-  //       branch: 0 // Branch number (used for colouring the line)
-  //     }
-  //   ]
-  // },
-  // {
-  //   position: 0, // X-axis location for the commit in this row
-  //   routes: [
-  //     {
-  //       from: 0, // X-axis of the starting location for the branch line
-  //       to: 0, // X-axis for the finishing location for the branch line
-  //       branch: 1 // Branch number (used for colouring the line)
-  //     }
-  //   ]
-  // }
-  // {
-  //   position: 1, // X-axis location for the commit in this row
-  //   routes: [
-  //     {
-  //       from: 1, // X-axis of the starting location for the branch line
-  //       to: 1, // X-axis for the finishing location for the branch line
-  //       branch: 0 // Branch number (used for colouring the line)
-  //     }
-  //   ]
-  // }
-]
-*/
 
-const graph = [
-  {
-    sha: '5fcbfdb998b69bcec729f79f346d34e8f20c642f',
-    offset: 0,
-    branch: 0,
-    routes: [[0, 0, 0]]
-  },
-  {
-    sha: 'e8438a0217eff657ac47cec2816003c16182b653',
-    offset: 1,
-    branch: 1,
-    routes: [[0, 0, 0], [1, 1, 1]]
-  },
-  {
-    sha: '0f2eeef1329333084a158541d80e6fc538462a48',
-    offset: 1,
-    branch: 1,
-    routes: [[0, 0, 0], [1, 1, 1]]
-  },
-  {
-    sha: '7d8835ec29452540b646e192937de2bc1a49cbaf',
-    offset: 1,
-    branch: 1,
-    routes: [[0, 0, 0], [1, 1, 1]]
-  },
-  {
-    sha: '01724ba5aafc3c3a5df3d43d24d7c4d2fdcb7d9c',
-    offset: 1,
-    branch: 1,
-    routes: [[0, 0, 0], [1, 1, 1]]
-  },
-  {
-    sha: '53797fdfcaa1d06a32f9abd61360fb354ed51929',
-    offset: 1,
-    branch: 1,
-    routes: [[0, 0, 0], [1, 1, 1]]
-  },
-  {
-    sha: '41ad84fa675406e30854835b2ebde8aa61e226b6',
-    offset: 1,
-    branch: 1,
-    routes: [[0, 0, 0], [1, 1, 1]]
-  },
-  {
-    sha: 'efaf2871c5e3086482b93aa2c27547a0854e000e',
-    offset: 1,
-    branch: 1,
-    routes: [[0, 0, 0], [1, 1, 1]]
-  },
-  {
-    sha: 'd50f88b728a82bb6451e0f07f1c53842c8b9aec1',
-    offset: 1,
-    branch: 1,
-    routes: [[0, 0, 0], [1, 1, 1]]
-  },
-  {
-    sha: '76708c336be8e3b094d963ce68ff5680bdd82866',
-    offset: 1,
-    branch: 1,
-    routes: [[0, 0, 0], [1, 1, 1]]
-  },
-  {
-    sha: '3d32a13255de9b37c5b0b568619d1eeebf5ab06d',
-    offset: 1,
-    branch: 1,
-    routes: [[0, 0, 0], [1, 1, 1]]
-  },
-  {
-    sha: 'f718b0207ca5212f728dd2b879d0a6515976e470',
-    offset: 1,
-    branch: 1,
-    routes: [[0, 0, 0], [1, 1, 1]]
+const processCommits = data => {
+  let branchIndex = 0
+  const reserve = []
+  const branches = {}
+  const getBranch = sha => {
+    if (branches[sha] == null) {
+      branches[sha] = branchIndex
+      reserve.push(branchIndex)
+      branchIndex += 1
+    }
+
+    return branches[sha]
   }
-]
+
+  return data.map(({ sha, parents }) => {
+    const [parent, otherParent] = parents
+
+    const branch = getBranch(sha)
+    const offset = reserve.indexOf(branch)
+
+    let routes = []
+
+    if (parents.length === 1) {
+      if (branches[parent] == null) {
+        // прямой путь
+        routes = [...fillRoutes(I, I, reserve)]
+        // единственный предок будет в том же столбце - передаем трекинг
+        branches[parent] = branch
+      } else {
+        routes = [
+          // все возможные ветки правее текущей загибаем влево
+          ...fillRoutes(i => i + offset + 1, i => i + offset + 1 - 1, reserve.slice(offset + 1)),
+          // все возможные ветки левее текущей продолжают идти параллельно
+          ...fillRoutes(I, I, reserve.slice(0, offset))
+        ]
+
+        // удаляем текущую ветку из списка
+        reserve.splice(offset, 1)
+
+        // загибаем текущую ветку в сторону ее родителя
+        routes = [...routes, [offset, reserve.indexOf(branches[parent]), branch]]
+      }
+    } else if (parents.length === 2) {
+      if (branches[parent] == null) {
+        branches[parent] = branch
+
+        routes = [...routes, ...fillRoutes(I, I, reserve)]
+      } else {
+        const parentOffset = reserve.indexOf(branches[parent])
+        if (parentOffset !== offset) {
+          // загибаем ветку в сторону родителя
+          routes = [...routes, [offset, parentOffset, branch]]
+
+          routes = [
+            ...routes,
+            // все возможные ветки правее текущей загибаем влево
+            ...fillRoutes(i => i + offset + 1, i => i + offset + 1 - 1, reserve.slice(offset + 1)),
+            // все возможные ветки левее текущей продолжают идти параллельно
+            ...fillRoutes(I, I, reserve.slice(0, offset))
+          ]
+
+          reserve.splice(offset, 1)
+        }
+      }
+
+      routes = [...routes, ...fillRoutes(I, I, reserve)]
+
+      const otherBranch = getBranch(otherParent)
+      routes = [...routes, [offset, reserve.indexOf(otherBranch), otherBranch]]
+    }
+
+    // удаляем ветку из кеша (на нее никто не ссылается больше)
+    delete branches[sha]
+
+    return {
+      sha,
+      offset,
+      branch,
+      routes
+    }
+  })
+}
 
 export const GitLog = () => {
   const canvasRef = useRef(null)
@@ -224,11 +205,8 @@ export const GitLog = () => {
     const ctx = canvas.getContext('2d')
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
 
-    // ctx.beginPath()
-    // ctx.arc(95, 50, 40, 0, 2 * Math.PI)
-    // ctx.stroke()
-    drawGraph(ctx, graph)
+    drawGraph(ctx, 0, processCommits(graph))
   }, [])
 
-  return <canvas ref={canvasRef} width={500} height={500} />
+  return <CanvasStyle ref={canvasRef} width={500} height={500} />
 }
---
+++ src/graph.js
@@ -0,0 +1,85 @@
+// export default [
+//   {
+//     sha: 'H',
+//     parents: ['F']
+//   },
+//   {
+//     sha: 'I',
+//     parents: ['B']
+//   },
+//   {
+//     sha: 'G',
+//     parents: ['E']
+//   },
+//   {
+//     sha: 'F',
+//     parents: ['D', 'E']
+//   },
+//   {
+//     sha: 'E',
+//     parents: ['C']
+//   },
+//   {
+//     sha: 'D',
+//     parents: ['C']
+//   },
+//   {
+//     sha: 'C',
+//     parents: ['A']
+//   },
+//   {
+//     sha: 'B',
+//     parents: ['A']
+//   },
+//   {
+//     sha: 'A',
+//     parents: []
+//   }
+// ]
+
+export default [
+  {
+    sha: 'C3',
+    parents: ['9C']
+  },
+  {
+    sha: 'C0',
+    parents: ['93', '9C']
+  },
+  {
+    sha: '9C',
+    parents: ['C5', '64']
+  },
+  {
+    sha: 'C5',
+    parents: ['C2', '24']
+  },
+  {
+    sha: '93',
+    parents: ['F4', '64']
+  },
+  {
+    sha: '64',
+    parents: ['C2', '24']
+  },
+  {
+    sha: '24',
+    parents: ['C2']
+  },
+  {
+    sha: 'F4',
+    parents: ['25', 'C2']
+  },
+  {
+    sha: 'C2',
+    parents: ['35']
+  },
+  {
+    sha: '25',
+    parents: ['35']
+  },
+  {
+    sha: '35',
+    parents: []
+  }
+]
